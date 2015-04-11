0cf63a42103d88fb3ea26272dfe2befd6f4b07ce
Jon Seymour <jon.seymour@gmail.com>
2015-04-11 14:02:43 +1000

    Run websocket server within sphere-ui process.
    Signed-off-by: Jon Seymour <jon.seymour@gmail.com>

--- main.go
+++ main.go
@@ -7,17 +7,30 @@ import (
 	"github.com/ninjasphere/go-ninja/config"
 	"github.com/ninjasphere/go-ninja/logger"
 	"github.com/ninjasphere/go-ninja/support"
+	"github.com/ninjasphere/sphere-ui/websocket"
 )
 
-var log = logger.GetLogger("sphere-ui")
+var log *logger.Logger
 
 func main() {
+
+	log = logger.GetLogger("sphere-ui")
 	fs := http.FileServer(http.Dir("public"))
 	http.Handle("/", fs)
 
 	address := fmt.Sprintf(":%d", config.Int(80, "sphere-ui.port"))
 	log.Infof("Listening on %s", address)
-	http.ListenAndServe(address, nil)
 
+	ws := websocket.WebsocketServer{
+		Port: config.Int(9001, "sphere-ui.websocket.port"),
+	}
+	if err := ws.PostConstruct(); err != nil {
+		log.FatalErrorf(err, "failed while starting websocket server")
+	}
+
+	if err := http.ListenAndServe(address, nil); err != nil {
+		log.FatalErrorf(err, "failed while listening http server")
+	}
 	support.WaitUntilSignal()
+
 }
